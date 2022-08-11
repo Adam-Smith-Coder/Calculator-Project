2c1f2a6454d6f7de785c3e2dc9f5997e7941ec5f
Adam-Smith-Coder <adcollects@gmail.com>
2022-08-11 10:53:43 +0100

    Update code to include alert messages to stop errors
    If someone tried to divide by 0 add an alert error message and change the
    display to blank
    If someone presses = when the display reads 9 + then display an error
    citing incomplete code but leave the previously typed numbers and operator
    Add new class to the . button called disable and create a function
    to disable this when it is pressed once
    Remove disable from . when an operator is pressed or = is pressed to stop
    someone typing 5.7.8 but allow them to put 5.7 + 6.8

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll('#btn');
 const display = document.getElementById('display');
 const answer = document.getElementById('answer');
-let displayText = display.textContent;
+const disableBtn = document.querySelector('.disable');
 
 buttons.forEach((button) => {
     let numbers = '0123456789.';
@@ -10,15 +10,29 @@ buttons.forEach((button) => {
             display.insertAdjacentText("beforeend", `${button.textContent}`);
         } else if (!numbers.includes(button.textContent)){
             display.insertAdjacentText("beforeend", ` ${button.textContent} `);
+            disableBtn.disabled = false
         }
         midCheck();
     });
 });
 
+const disableButton = () => {
+    disableBtn.disabled = true;
+}
+
+
+disableBtn.addEventListener('click', 
+disableButton);
+
 document.getElementById('calculate').onclick = function() {
     let displayText = display.textContent;
     let displayCalc = displayText.split(' ');
-    operate(displayCalc); 
+    if (displayCalc[2] === '') {
+        alert `Error unfinished calculation`
+    } else {
+        operate(displayCalc); 
+    }
+    disableBtn.disabled = false
 }
 
 document.getElementById('clear').onclick = function() {
@@ -76,6 +90,7 @@ let subtract = function(nums) {
     display.textContent = Number(sum.toFixed(2));
 }
 
+
 let multiply = function(nums) {
     let sum = 1;
 
@@ -89,15 +104,20 @@ let multiply = function(nums) {
 }
 
 let divide = function(nums) {
-    let sum = nums[0];
+    if (nums.includes(0)) {
+       alert `Error you tried to divide by 0`;
+       answer.textContent = "";
+       display.textContent = "";
+    } else {
+        let sum = nums[0];
 
     for (let i = 1; i < nums.length; i++) {
         let currNum = nums[i];
         sum /= currNum;
     }
-
     answer.textContent = `${display.textContent} = ${Number(sum.toFixed(2))}`;
     display.textContent = Number(sum.toFixed(2));
+    }
 }
 
 let midCheck = function () {
@@ -107,7 +127,11 @@ let midCheck = function () {
         let firstCalc = displayCalc.slice(0, 3);
         let remainder = displayCalc.slice(3, 4);
         operate(firstCalc); 
-        answer.textContent = `${firstCalc.join(' ')} = ${display.textContent}`;
-        display.insertAdjacentText("beforeend", ` ${remainder} `);
+        if (answer.textContent === "" && display.textContent === "") {
+
+        } else {
+            answer.textContent = `${firstCalc.join(' ')} = ${display.textContent}`;
+            display.insertAdjacentText("beforeend", ` ${remainder} `);
+        }
     }
 }
